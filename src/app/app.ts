a36056350ca0320c49953aa79bc424412eea8ec7
Fabricio Silva <fabricionhsilva@gmail.com>
2025-12-14 19:16:44 -0300

    feat: criação de componente próprio para a sidebar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,36 +1,16 @@
 import {Component, signal} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Sidebar} from './components/sidebar/sidebar';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [
+        Sidebar
+    ],
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
 export class App {
+
     protected readonly title = signal('admin-template');
 
-    isCollapsed = signal(false);
-
-    toggleMenu(): void {
-        this.isCollapsed.update(v => !v);
-    }
-
-    menuItems: MenuItem[] = [
-        {id: 1, label: 'Dashboard', path: '', icon: 'ph ph-house', active: true},
-        {id: 2, label: 'Team', path: '', icon: 'ph ph-users-three'},
-        {id: 3, label: 'Analytics', path: '', icon: 'ph ph-chart-line'},
-        {id: 4, label: 'Documents', path: '', icon: 'ph ph-files'},
-        {id: 5, label: 'Calendar', path: '', icon: 'ph ph-calendar'},
-        {id: 6, label: 'Messages', path: '', icon: 'ph ph-chat'},
-        {id: 7, label: 'Settings', path: '', icon: 'ph ph-gear'},
-    ];
-}
-
-interface MenuItem {
-    id: number;
-    label: string;
-    icon: string;
-    active?: boolean;
-    path: string;
 }
---
+++ src/app/components/sidebar/sidebar.ts
@@ -0,0 +1,35 @@
+import {Component, signal} from '@angular/core';
+
+interface MenuItem {
+    id: number;
+    label: string;
+    icon: string;
+    active?: boolean;
+    path: string;
+}
+
+@Component({
+  selector: 'sidebar',
+  imports: [],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {
+
+    isCollapsed = signal(false);
+
+    toggleMenu(): void {
+        this.isCollapsed.update(v => !v);
+    }
+
+    menuItems: MenuItem[] = [
+        {id: 1, label: 'Dashboard', path: '', icon: 'ph ph-house', active: true},
+        {id: 2, label: 'Team', path: '', icon: 'ph ph-users-three'},
+        {id: 3, label: 'Analytics', path: '', icon: 'ph ph-chart-line'},
+        {id: 4, label: 'Documents', path: '', icon: 'ph ph-files'},
+        {id: 5, label: 'Calendar', path: '', icon: 'ph ph-calendar'},
+        {id: 6, label: 'Messages', path: '', icon: 'ph ph-chat'},
+        {id: 7, label: 'Settings', path: '', icon: 'ph ph-gear'},
+    ];
+
+}
