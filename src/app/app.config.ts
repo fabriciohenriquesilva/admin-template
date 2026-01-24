fcb82d33c8e7200dd590d08498d9edeb668ed45c
Fabricio Silva <fabricionhsilva@gmail.com>
2026-01-24 20:20:09 -0300

    fix: ajuste no tema da aplicação

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -9,7 +9,7 @@ import {definePreset} from '@primeuix/themes';
 const myPreset = definePreset(Aura, {
     semantic: {
         primary: {
-            50:  '{blue.50}',
+             50: '{blue.50}',
             100: '{blue.100}',
             200: '{blue.200}',
             300: '{blue.300}',
@@ -24,24 +24,24 @@ const myPreset = definePreset(Aura, {
         colorScheme: {
             light: {
                 surface: {
-                    0: '#ffffff',
-                    50: '{zinc.50}',
-                    100: '{zinc.100}',
-                    200: '{zinc.200}',
-                    300: '{zinc.300}',
-                    400: '{zinc.400}',
-                    500: '{zinc.500}',
-                    600: '{zinc.600}',
-                    700: '{zinc.700}',
-                    800: '{zinc.800}',
-                    900: '{zinc.900}',
-                    950: '{zinc.950}'
+                      0: '#ffffff',
+                     50: '{gray.50}',
+                    100: '{gray.100}',
+                    200: '{gray.200}',
+                    300: '{gray.300}',
+                    400: '{gray.400}',
+                    500: '{gray.500}',
+                    600: '{gray.600}',
+                    700: '{gray.700}',
+                    800: '{gray.800}',
+                    900: '{gray.900}',
+                    950: '{gray.950}'
                 }
             },
             dark: {
                 surface: {
-                    0: '#ffffff',
-                    50: '{slate.50}',
+                      0: '#ffffff',
+                     50: '{slate.50}',
                     100: '{slate.100}',
                     200: '{slate.200}',
                     300: '{slate.300}',
