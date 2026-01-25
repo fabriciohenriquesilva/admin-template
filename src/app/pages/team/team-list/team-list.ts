d36be33194b2313d55f84fb20877f6ac006bb64d
Fabricio Silva <fabricionhsilva@gmail.com>
2026-01-25 16:12:34 -0300

    fix: ajuste em modelo

--- src/app/pages/team/team-list/team-list.ts
+++ src/app/pages/team/team-list/team-list.ts
@@ -16,23 +16,23 @@ import {Tag} from 'primeng/tag';
 })
 export class TeamList {
 
-    products: Product[] = [];
+    teamList: Team[] = [];
     cols: any[] = [];
     mapToStatus: Map<string, string> = new Map<string, string>();
 
     constructor() {
-        this.products = [
-            {id: 1, name: 'Product 1', category: 'Category 1', quantity: 10, status: 'Approved'},
-            {id: 2, name: 'Product 2', category: 'Category 2', quantity: 20, status: 'Awaiting'},
-            {id: 3, name: 'Product 3', category: 'Category 3', quantity: 30, status: 'No Status'},
-            {id: 4, name: 'Product 4', category: 'Category 4', quantity: 40, status: 'Approved'},
-            {id: 5, name: 'Product 5', category: 'Category 5', quantity: 50, status: 'Approved'},
-            {id: 6, name: 'Product 6', category: 'Category 4', quantity: 60, status: 'Rejected'},
-            {id: 7, name: 'Product 7', category: 'Category 3', quantity: 70, status: 'No Status'},
-            {id: 8, name: 'Product 8', category: 'Category 2', quantity: 80, status: 'Pending'},
-            {id: 9, name: 'Product 9', category: 'Category 1', quantity: 90, status: 'Pending'},
-            {id: 10, name: 'Product 10', category: 'Category 4', quantity: 0, status: 'Rejected'},
-            {id: 11, name: 'Product 11', category: 'Category 3', quantity: 10, status: 'Rejected'},
+        this.teamList = [
+            {id: 1, name: 'Team 1', category: 'Category 1', quantity: 10, status: 'Approved'},
+            {id: 2, name: 'Team 2', category: 'Category 2', quantity: 20, status: 'Awaiting'},
+            {id: 3, name: 'Team 3', category: 'Category 3', quantity: 30, status: 'No Status'},
+            {id: 4, name: 'Team 4', category: 'Category 4', quantity: 40, status: 'Approved'},
+            {id: 5, name: 'Team 5', category: 'Category 5', quantity: 50, status: 'Approved'},
+            {id: 6, name: 'Team 6', category: 'Category 4', quantity: 60, status: 'Rejected'},
+            {id: 7, name: 'Team 7', category: 'Category 3', quantity: 70, status: 'No Status'},
+            {id: 8, name: 'Team 8', category: 'Category 2', quantity: 80, status: 'Pending'},
+            {id: 9, name: 'Team 9', category: 'Category 1', quantity: 90, status: 'Pending'},
+            {id: 10, name: 'Team 10', category: 'Category 4', quantity: 0, status: 'Rejected'},
+            {id: 11, name: 'Team 11', category: 'Category 3', quantity: 10, status: 'Rejected'},
         ];
 
         this.mapToStatus = new Map<string, string>([
@@ -48,12 +48,4 @@ export class TeamList {
         return this.mapToStatus.get(status) ?? 'secondary';
     }
 
-}
-
-interface Product {
-    id?: number;
-    name: string;
-    category: string;
-    quantity: number;
-    status: string;
 }
